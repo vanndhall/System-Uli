31e56b79b01d01f1eba19275e09b58729608fb62
vanndhall <szymon.mantey@wp.pl>
2018-04-04 22:15:06 +0200

    ClassWorker part 1

---
+++ System Uli/Worker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_Uli
+{
+    class Worker
+    {
+        #region BUILDER
+        public Worker(string[] jobsICanDo)
+        {
+            this.jobsICanDo = jobsICanDo;
+        }
+        #endregion
+
+        #region PROPERTIES
+        public int ShiftsLeft
+        { 
+            get
+                { return shiftsToWork - shiftsWorked; }
+        }
+        public string CurrentJob
+        {
+            get
+                { return currentJob; }
+        }
+        
+
+
+        #endregion
+
+        #region VARIABLES
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+        private string currentJob = "";
+        #endregion
+
+        #region METHODS
+
+
+
+
+        #endregion
+
+
+    }
+}
