92dc7aa0d2bfc650c56209c46879f6be3ec9d4f1
vanndhall <szymon.mantey@wp.pl>
2018-04-04 21:36:22 +0200

    GUIDone

--- System Uli/Form1.Designer.cs
+++ System Uli/Form1.Designer.cs
@@ -28,20 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.report = new System.Windows.Forms.RichTextBox();
+            this.nextShift = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // workerBeeJob
+            // 
+            this.workerBeeJob.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.workerBeeJob.FormattingEnabled = true;
+            this.workerBeeJob.Items.AddRange(new object[] {
+            "Zbieranie nektaru",
+            "Pielęgnacja jaj",
+            "Utrzymanie ula",
+            "Wytwarzanie miodu",
+            "Patrol z żądłami",
+            "Nauczanie pszczółek",
+            "Wytwarzanie miodu"});
+            this.workerBeeJob.Location = new System.Drawing.Point(17, 19);
+            this.workerBeeJob.Name = "workerBeeJob";
+            this.workerBeeJob.Size = new System.Drawing.Size(121, 21);
+            this.workerBeeJob.TabIndex = 0;
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(158, 19);
+            this.shifts.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(120, 20);
+            this.shifts.TabIndex = 1;
+            this.shifts.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.workerBeeJob);
+            this.groupBox1.Controls.Add(this.shifts);
+            this.groupBox1.Location = new System.Drawing.Point(11, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 114);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Przydział prac robotnicom";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Przypisz tę pracę pszczole";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(11, 132);
+            this.report.Name = "report";
+            this.report.Size = new System.Drawing.Size(417, 199);
+            this.report.TabIndex = 3;
+            this.report.Text = "";
+            // 
+            // nextShift
+            // 
+            this.nextShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nextShift.Location = new System.Drawing.Point(304, 19);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(124, 107);
+            this.nextShift.TabIndex = 4;
+            this.nextShift.Text = "Przepracuj następną zmianę";
+            this.nextShift.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(819, 551);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(440, 340);
+            this.Controls.Add(this.nextShift);
+            this.Controls.Add(this.report);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "System zarządzania ulem";
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox workerBeeJob;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox report;
+        private System.Windows.Forms.Button nextShift;
     }
 }
 
