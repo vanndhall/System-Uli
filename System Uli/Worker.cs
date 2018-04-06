8cbcbe45d883252639ba9cdff3023a14c7947233
vanndhall <szymon.mantey@wp.pl>
2018-04-06 21:58:11 +0200

    DidYouFinish()

--- System Uli/Worker.cs
+++ System Uli/Worker.cs
@@ -39,11 +39,14 @@ namespace System_Uli
         #endregion
 
         #region METHODS
+        //Metoda do przypisywania nowych zadań przez Queen, sprawdza ich pole jobsICanDo aby okreslic,
+        //czy dana pszczoła potrafi wykonywać ten rodzaj pracy
         public bool DoThisJob(string job, int numberOfShifts)
         {
 
             if (!String.IsNullOrEmpty(currentJob)) // guard
-                return false;
+                return false;                       //
+
             for (int i = 0; i < jobsICanDo.Length; i++)
                 if(jobsICanDo[i] == job)
                 {
@@ -57,6 +60,28 @@ namespace System_Uli
             return false;
         }
        
+
+        //Metoda do nakazania im rozpoczecie kolejnej zmian;
+        //Zwraca ona true tylko wtedy, gdy jest to naprawdę ostatnia zmiana z przeznaczonych na wykonanie zleconej pracy.
+        //Dzięki temu królowa może dodać do rapotu linijkę i poinformować, że pszczoła po tej zmianie zakończy swoją pracę.
+        public bool DidYuFinish()
+        {
+            if (String.IsNullOrEmpty(currentJob))  //Guard
+                return false;                       //
+
+            shiftsWorked++;
+            if (shiftsWorked > shiftsToWork)
+            {
+                shiftsWorked = 0;
+                shiftsToWork = 0;
+                currentJob = "";
+                return true;
+
+            }
+
+            else
+                return false;
+        }
 
 
 
