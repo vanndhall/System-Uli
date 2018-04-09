47f6f818c46b8f6153c806de0b79870719aa5cb1
vanndhall <szymon.mantey@wp.pl>
2018-04-09 22:22:54 +0200

    AssignWork

--- System Uli/Queen.cs
+++ System Uli/Queen.cs
@@ -8,6 +8,37 @@ namespace System_Uli
 {
     class Queen
     {
+        #region Konstruktor
+
+        public Queen(Worker[] workers)
+        {
+            this.workers = workers; 
+        }
+
+        #endregion
+
+        #region Pola
+        private Worker[] workers;
+        private int shiftNumber = 0;  //numer zmiany
+        #endregion
+
+        #region Metody
+
+            //przypisz pracę(praca , numer zmiany)
+        public bool AssignWork(string job, int numberOfShifts) 
+        {
+            for(int i = 0; i< workers.Length; i++)
+            {
+                if (workers[i].DoThisJob(job, numberOfShifts))
+                    return true;
+            }
+            return false;
+        }
+
+        //pracować na następnej zmianie
+
+        #endregion
+
 
     }
 }
